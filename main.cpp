a1da85c4c8819d62058e837bacfeef903939fa98
Stringybell <christianmartinpileri@gmail.com>
2025-10-13 13:03:19 +0200

    Added functions matrix_maker and free_matrix

--- main.cpp
+++ main.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
 
+using namespace std;
+
+// Creates a matrix of pointers with n rows and n columns
+
+bool** matrix_maker(int rows, int columns){
+
+    bool** matrix = new bool * [rows];
+
+    for (int i = 0; i < rows ; i++){
+        matrix[i] = new bool[columns];
+    }
+
+// Initialises the matrix as false
+    for (int i = 0; i < rows ; i++){
+        for (int x = 0; x < columns; x++){
+            matrix[i][x] = false;
+        }
+    }
+
+
+    return matrix;
+
+}
+void free_matrix(bool** matrix, int rows){
+    for (int i = 0; i < rows; i++){
+        delete matrix [i];
+    }
+    delete matrix;
+}
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int rows,columns;
+    cin >> rows >> columns;
+
+    bool** matrix = matrix_maker(rows,columns);
+
+    for (int i = 0; i < rows ; i++){
+        for (int x = 0; x < columns; x++){
+            cout << matrix[i][x];
+        }
+        cout << "\n" ;
+    }
+
+    free_matrix(matrix, rows);
+
+
     return 0;
 }
